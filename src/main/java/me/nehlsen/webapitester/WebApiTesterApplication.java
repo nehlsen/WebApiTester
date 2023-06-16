d02329b0440991c0e0dca0a3a630fcb07553245b
Nils Weigel <nils.weigel@gmail.com>
2023-06-16 19:08:27 +0200

    initialized project

---
+++ src/main/java/me/nehlsen/webapitester/WebApiTesterApplication.java
@@ -0,0 +1,13 @@
+package me.nehlsen.webapitester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApiTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nehlsen/webapitester/WebApiTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nehlsen.webapitester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
